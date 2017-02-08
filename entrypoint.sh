c3b95994a4cb95ace7716edc98b9ebceb9a26520
slimatic <salim.31@gmail.com>
2017-02-08 12:22:10 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 aws configure set aws_access_key_id ${ACCESS_KEY}
 aws configure set aws_secret_access_key ${SECRET_KEY}
 aws configure set region ${REGION}
-aws s3 sync ${S3_PATH} /var/www
+# aws s3 sync ${S3_PATH} /var/www
+
+cp -fr /var/artifacts/* /var/www
